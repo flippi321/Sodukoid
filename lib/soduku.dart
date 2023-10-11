68a671731067b650c0310c1ebe4826b64c641f0f
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-11 19:03:20 +0200

    Updates isValidMove to check locked Squares and the 3x3 square our placement is in

--- lib/soduku.dart
+++ lib/soduku.dart
@@ -1,5 +1,7 @@
 class Sudoku {
   late List<List<int>> board;
+  late List<List<bool>> lockedSquares;
+  late String solutionLocation;
 
   Sudoku() {
     board = List.generate(9, (i) => List.generate(9, (j) => 0));
@@ -18,13 +20,29 @@ class Sudoku {
   }
 
   bool isValidMove(int row, int col, int value) {
+    // If the given value is locked, we cannot overwrite the value
+    if(lockedSquares[row][col]){
+      return false;
+    }
+
+    // Parse every square sharing the same row or column
     for (int i = 0; i < 10; i++){
-      // If any square on the same row OR colon shares the same value it's invalid
+      // If the square shares the same value, this move is invalid
       if((board[row][i] == value && i != col) || (board[i][col] == value && i != row)){
         return false;
       }
     }
+
+    // Check the 3x3 square the value is in
+    int startRow = row - (row % 3);
+    int startCol = col - (col % 3);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[startRow + i][startCol + j] == value && (startRow + i != row || startCol + j != col)) {
+          return false;
+        }
+      }
+    }
     return true;
   }
-
 }
