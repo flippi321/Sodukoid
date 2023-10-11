6f0630faf982bdd69ff206b26f9ce392f199cc79
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-11 18:56:01 +0200

    Added method checking that a given move is valid

--- lib/soduku.dart
+++ lib/soduku.dart
@@ -19,8 +19,9 @@ class Sudoku {
 
   bool isValidMove(int row, int col, int value) {
     for (int i = 0; i < 10; i++){
-      if(){
-
+      // If any square on the same row OR colon shares the same value it's invalid
+      if((board[row][i] == value && i != col) || (board[i][col] == value && i != row)){
+        return false;
       }
     }
     return true;
