c004f5b83dda9302485e7b163650da512f86be78
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-18 21:39:59 +0200

    Added a reusable appbar widget

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final Function onPressedRefresh;
+
+  const CustomAppBar({Key? key, required this.title, required this.onPressedRefresh})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      backgroundColor: Colors.transparent,
+      flexibleSpace: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: IconButton(
+            icon: const Icon(Icons.refresh, color: Colors.black),
+            onPressed: onPressedRefresh as void Function()?,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
