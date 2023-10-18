9240e6e4450d9da4da723f9872aab3f5e4d689d7
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-18 22:39:45 +0200

    Added text from the tutorial page

--- lib/provider/language_provider.dart
+++ lib/provider/language_provider.dart
@@ -19,12 +19,29 @@ class LanguageProvider with ChangeNotifier {
       'newGame': 'New Game',
       'createBoard': 'Create a Board',
       'howToPlay': 'How to Play',
-
+      'howToPlay2': "1. Click on a square and it turns blue.\n"
+          "2. Click on a number at the bottom of the screen to assign that number to the selected square.\n"
+          "3. You can color a square in three ways:\n"
+          "   - White (Standard)\n"
+          "   - Yellow (Unsure)\n"
+          "   - Green (Definitely correct)\n"
+          "4. Press the lightbulb icon to show all incorrect squares.",
+      'sodukuRules': 'Soduku Rules',
+      'sodukuRules2': "Sudoku is a logic-based number puzzle. The objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 subgrids that compose the grid contain all of the digits from 1 to 9. Each puzzle has a unique solution and no math is required to solve it. Just pure logic and deduction.",
     },
     'no': {
-      'newGame': 'Start et spill',
-      'createBoard': 'Lag et nytt brett',
-      'howToPlay': 'Hvordan spiller du',
+      'newGame': 'Nytt spill',
+      'createBoard': 'Lag et brett',
+      'howToPlay': 'Hvordan spille',
+      'howToPlay2': "1. Klikk på et felt og det blir blått.\n"
+          "2. Klikk på et nummer nederst på skjermen for å tildele det nummeret til det valgte feltet.\n"
+          "3. Du kan fargelegge et felt på tre måter:\n"
+          "   - Hvit (Standard)\n"
+          "   - Gul (Usikker)\n"
+          "   - Grønn (Helt riktig)\n"
+          "4. Trykk på lyspæreikonet for å vise alle feil felter.",
+      'sodukuRules': 'Sudoku-regler',
+      'sodukuRules2': "Sudoku er et logikkbasert tallpuslespill. Målet er å fylle et 9×9 rutenett med sifre slik at hver kolonne, hver rad og hver av de ni 3×3 delrutene som utgjør rutenettet inneholder alle sifrene fra 1 til 9. Hvert puslespill har en unik løsning, og det kreves ingen matte for å løse det. Bare ren logikk og deduksjon.",
     },
   };
 }
