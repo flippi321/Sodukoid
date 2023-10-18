680b298758240e43f8fd2901ada91c7ff68fc868
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-18 22:14:55 +0200

    Added language icons in top right corner

--- lib/home.dart
+++ lib/home.dart
@@ -17,81 +17,114 @@ class SudokuHomePage extends StatelessWidget {
             colors: [Colors.orange, Colors.blue],
           ),
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          children: [
+            // Positioned Flags
+            Positioned(
+              top: 40,
+              right: 20,
+              child: Row(
                 children: [
-                  Image.asset(
-                    'assets/images/logo.png',
-                    height: 50.0,
+                  GestureDetector(
+                    onTap: () {
+                      // TODO change language to norwegian
+                    },
+                    child: Image.asset(
+                      'assets/images/norway_flag.png',
+                      height: 30.0,
+                    ),
                   ),
                   const SizedBox(width: 10),
-                  const Text(
-                    "Sodukoid",
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                  GestureDetector(
+                    onTap: () {
+                      // TODO change language to english
+                    },
+                    child: Image.asset(
+                      'assets/images/uk_flag.png',
+                      height: 30.0,
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const NewGamePage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                ),
-                child: const Text('New Game'),
+            ),
+            // Existing Content
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/logo.png',
+                        height: 50.0,
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        "Sodukoid",
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const NewGamePage()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white,
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    ),
+                    child: const Text('New Game'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const CreateSudokuBoardPage()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white,
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    ),
+                    child: const Text('Create a Board'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SudokuTutorialPage()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white,
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    ),
+                    child: const Text('How to Play'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const CreateSudokuBoardPage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                ),
-                child: const Text('Create a Board'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SudokuTutorialPage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                ),
-                child: const Text('How to Play'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
