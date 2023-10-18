9194e463d81550301e0e6f699fe8e8d7ba7ec273
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-10-18 21:32:52 +0200

    Created a victory page for when a puzzle is solved

---
+++ lib/soduku_congratulations_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:soduku_app/home.dart';
+
+class SodukuCongratulationsScreen extends StatelessWidget {
+  const SodukuCongratulationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Congratulations!'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.orange, Colors.blue],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Congratulations on solving the Sudoku puzzle!',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SudokuHomePage()));
+                },
+                child: const Text('Back to Home'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
